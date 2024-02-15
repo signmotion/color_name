aa20ced8df8943866b3beda2d809b3f0cc5db813
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-15 20:55:32 +0100

    Extended ColorModel.

--- lib/src/color_model.dart
+++ lib/src/color_model.dart
@@ -2,15 +2,21 @@ part of '../uni_color_name.dart';
 
 enum ColorModel {
   unspecified,
+
   cmyk,
 
-  /// RRGGBB as int, 24 bits
+  /// RRGGBB as double, range [0.0; 1.0] per channel
   rgb,
 
-  /// "#RRGGBB"
-  rgbHexStr,
+  /// RRGGBB as int, range [0; 255] per channel
+  rgbInt8,
+
+  /// RRGGBB as hexdecimal string with prefix `#`
+  rgbString,
 
   hsl,
+
   hsv,
+
   xyz,
 }
