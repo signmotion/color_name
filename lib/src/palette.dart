daf909b534d586743dcc9567f8ce3b780d3115db
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-19 21:13:26 +0100

    Introduced the factory UniPalette.list().

--- lib/src/palette.dart
+++ lib/src/palette.dart
@@ -24,24 +24,38 @@ abstract class Palette<C extends Object> {
 class UniPalette<T extends Object> extends Palette<UniColor<T>> {
   UniPalette(super.map);
 
+  /// Constructing from [File].
+  /// See [UniPalette.list].
   factory UniPalette.file(String path, ColorModel model) {
-    if (model != ColorModel.rgb) {
-      throw UnimplementedError();
-    }
-
     final l = WFile(path).readAsJsonListListT<int>();
     if (l == null) {
       throw ArgumentError('The file `$path` not found.');
     }
 
-    if (l.length < 2) {
-      throw ArgumentError(
-          'The palette file should contain 2 or more colors. We have: $l');
+    final list = l.map((c) => c.colorRgbToIntRgb);
+
+    return UniPalette.list(list as Iterable<T>, model);
+  }
+
+  /// Constructing from [Iterable].
+  factory UniPalette.list(Iterable<T> list, ColorModel model) {
+    if (model != ColorModel.rgb) {
+      throw UnimplementedError();
     }
 
-    final list =
-        l.map((c) => c.colorRgbToUniColorShort<T>().withModel(ColorModel.rgb));
-    final map = {for (final c in list) c.colorRgbToStringRgb: c};
+    if (list.length < 2) {
+      throw ArgumentError('The palette should contain 2 or more colors.'
+          ' We have: ${list.length} $list');
+    }
+
+    final map = <String, UniColor<T>>{};
+    for (final c in list) {
+      final entry = switch (c) {
+        int v => MapEntry(v.colorRgbToStringRgb, v.colorRgbToUniColorRgb<T>()),
+        _ => throw ArgumentError('$c'),
+      };
+      map[entry.key] = entry.value;
+    }
 
     return UniPalette(map);
   }
@@ -49,47 +63,47 @@ class UniPalette<T extends Object> extends Palette<UniColor<T>> {
   @override
   ColorModel get model => map.values.first.model;
 
+  // @override
+  // TODO(sign): (String, UniColor<T>) closest(UniColor<T> color, ColorDistance cd) {
+  //   _points ??= [
+  //     for (final entry in map.entries)
+  //       {
+  //         'x': entry.value.red,
+  //         'y': entry.value.green,
+  //         'z': entry.value.blue,
+  //         'name': entry.key,
+  //       }
+  //   ];
+
+  //   double distance(Map<dynamic, dynamic> a, Map<dynamic, dynamic> b) {
+  //     final aa = (model, a['x'] as T, a['y'] as T, a['z'] as T);
+  //     final bb = (model, b['x'] as T, b['y'] as T, b['z'] as T);
+
+  //     return cd.distance(aa, bb);
+  //   }
+
+  //   _kdtree ??= KDTree(_points!, distance, ['x', 'y', 'z']);
+
+  //   final nearest = _kdtree!.nearest(
+  //     {
+  //       'x': color.red,
+  //       'y': color.green,
+  //       'z': color.blue,
+  //     },
+  //     1,
+  //   );
+
+  //   final r = (nearest.single as List<dynamic>).first as Map<String, dynamic>;
+  //   final c = (model, r['x'] as T, r['y'] as T, r['z'] as T);
+
+  //   return (r['name'] as String, c);
+  // }
+
+  // KDTree? _kdtree;
+  // List<Map<String, dynamic>>? _points;
+
   @override
   (String, UniColor<T>) closest(UniColor<T> color, ColorDistance cd) {
-    _points ??= [
-      for (final entry in map.entries)
-        {
-          'x': entry.value.red,
-          'y': entry.value.green,
-          'z': entry.value.blue,
-          'name': entry.key,
-        }
-    ];
-
-    double distance(Map<dynamic, dynamic> a, Map<dynamic, dynamic> b) {
-      final aa = (model, a['x'] as T, a['y'] as T, a['z'] as T);
-      final bb = (model, b['x'] as T, b['y'] as T, b['z'] as T);
-
-      return cd.distance(aa, bb);
-    }
-
-    _kdtree ??= KDTree(_points!, distance, ['x', 'y', 'z']);
-
-    final nearest = _kdtree!.nearest(
-      {
-        'x': color.red,
-        'y': color.green,
-        'z': color.blue,
-      },
-      1,
-    );
-
-    final r = (nearest.single as List<dynamic>).first as Map<String, dynamic>;
-    final c = (model, r['x'] as T, r['y'] as T, r['z'] as T);
-
-    return (r['name'] as String, c);
-  }
-
-  KDTree? _kdtree;
-  List<Map<String, dynamic>>? _points;
-
-  /// See [closest].
-  (String, UniColor<T>) closestSlow(UniColor<T> color, ColorDistance<T> cd) {
     // respect JavaScript limitation
     final maxIntValue = pow(2, 53).round() - 1;
     var min = maxIntValue.toDouble();
