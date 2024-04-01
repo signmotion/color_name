9e77cfc520c6dec7a47f741b6b6937a8c615e5e7
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-01 22:35:48 +0200

    Fixed the example.

--- example/main.dart
+++ example/main.dart
@@ -5,8 +5,7 @@ import 'package:uni_color_name/uni_color_name.dart';
 /// See also `example/all_palettes` forlder with Flutter SDK example
 /// that visualize a palette.
 void main() {
-  final palette = UniPalette(zeplinColors);
-  final u = UniColorName(palette);
+  final u = UniColorName(zeplinPalette);
   print(u.value('forrest green'));
   print(u.name(RgbInt8Color.rgb(0x15, 0x44, 0x06)));
 }
