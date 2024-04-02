dab4bc70e52e93a1f3416f84e6c765fc50099117
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-02 21:06:26 +0200

    Fixed after upgrade.

--- test/color_distance_test.dart
+++ test/color_distance_test.dart
@@ -6,17 +6,17 @@ void main() {
     test('RgbColorSqrtDistance', () {
       const cd = RgbColorSqrtDistance();
 
-      final a = 0x000000.rgbInt8Color;
-      final b = 0xffffff.rgbInt8Color;
+      final a = 0x000000.rgbInt8ToRgbInt8Color;
+      final b = 0xffffff.rgbInt8ToRgbInt8Color;
       expect(cd.distance(a, b).roundToDecimals(2), 441.67);
 
       {
-        final c = 0x010203.rgbInt8Color;
+        final c = 0x010203.rgbInt8ToRgbInt8Color;
         expect(cd.distance(a, c).roundToDecimals(2), 3.74);
       }
 
       {
-        final c = 0xf1f2f3.rgbInt8Color;
+        final c = 0xf1f2f3.rgbInt8ToRgbInt8Color;
         expect(cd.distance(b, c).roundToDecimals(2), 22.56);
       }
     });
